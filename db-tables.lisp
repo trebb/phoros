ba4a7471955abb1d637c17f640d3fafa17812bfc
Bert Burgemeister <trebbu@googlemail.com>
2010-11-08 17:33:53 +0100

    DB table creation ok.

--- db-tables.lisp
+++ db-tables.lisp
@@ -1,17 +1,21 @@
 (in-package :phoros)
 
 (defun nuke-all-tables ()
-  "Drop all tables (except PostGIS helper tables) in current database."
+  "Drop all tables (except PostGIS helper tables) and sequences in current database."
   (let ((user-tables (list-tables))
+        (sequences (list-sequences))
         (system-tables '(:spatial-ref-sys :geometry-columns)))
     (dolist (system-table system-tables)
       (setf user-tables (remove system-table user-tables))) ; TODO: do we need to spare those?
     (dolist (user-table user-tables)
-      (execute (format nil "DROP TABLE IF EXISTS ~A CASCADE" (s-sql:to-sql-name user-table))))))
+      (execute (format nil "DROP TABLE IF EXISTS ~A CASCADE" (s-sql:to-sql-name user-table))))
+    (dolist (sequence sequences)
+      (execute (format nil "DROP SEQUENCE IF EXISTS ~A CASCADE" (s-sql:to-sql-name sequence))))))
 
 (defclass sys-user ()
   ((user-id
-    :col-type integer)
+    :col-type integer
+    :col-default (:nextval 'sys-user-id-seq))
    (user-name
     :col-type text
     :documentation "This one is used for authentication.")
@@ -23,20 +27,27 @@
   (:keys user-id)
   (:documentation "This is certainly not a full-fledged authentication system."))
 
-(deftable sys-user (!dao-def))
+(deftable sys-user
+  (:create-sequence 'sys-user-id-seq)
+  (!dao-def))
 
 (defclass sys-project ()
   ((project-id
-    :col-type integer)
+    :col-type integer
+    :col-default (:nextval 'sys-project-id-seq))
    (project-name
-    :col-type text)
-   (table-name
     :col-type text
-    :documentation "Should be one table for all projects but this seems to come with a speed penalty."))
+    :initarg :project-name)
+   (common-table-name
+    :col-type text
+    :initarg :common-table-name
+    :documentation "Name of this project's data tables sans their canonical prefixes and suffixes.  Should be one table for all projects but this seems to come with a speed penalty."))
   (:metaclass dao-class)
   (:keys project-id))
 
-(deftable sys-project (!dao-def))
+(deftable sys-project
+  (:create-sequence 'sys-project-id-seq)
+  (!dao-def))
 
 (defclass sys-user-role ()
   ((user-id
@@ -56,7 +67,8 @@
 
 (defclass sys-measurement ()
   ((measurement-id
-    :col-type integer)
+    :col-type integer
+    :col-default (:nextval 'sys-measurement-id-seq))
    (project-id
     :col-type integer)
    (directory
@@ -65,11 +77,14 @@
   (:metaclass dao-class)
   (:keys measurement-id))
 
-(deftable sys-measurement (!dao-def))
+(deftable sys-measurement
+  (:create-sequence 'sys-measurement-id-seq)
+  (!dao-def))
 
 (defclass sys-camera-hardware ()
   ((camera-hardware-id
-    :col-type integer)
+    :col-type integer
+    :col-default (:nextval 'sys-camera-hardware-id-seq))
    (sensor-width-pix
     :col-type integer)
    (sensor-height-pix
@@ -87,34 +102,40 @@
     :col-type integer[]
     :documentation "Array containing the colors the first pixels of the first two (or three) rows.  Each pixel is to be interpreted as a three-byte RGB value.")
    (serial-number
-    :col-type (or db-null text))
+    :col-type text)
    (description
-    :col-type (or db-null text)
+    :col-type text
     :description "Camera type, manufacturer, etc."))
   (:metaclass dao-class)
   (:keys camera-hardware-id))
 
-(deftable sys-camera-hardware (!dao-def))
+(deftable sys-camera-hardware
+  (:create-sequence 'sys-camera-hardware-id-seq)
+  (!dao-def))
 
 (defclass sys-lens ()
   ((lens-id
-    :col-type integer)
+    :col-type integer
+    :col-default (:nextval 'sys-lens-id-seq))
    (c
     :col-type double-float
     :documentation "Focal length.  Only for human consumption.")
    (serial-number
-    :col-type (or db-null text))
+    :col-type text)
    (description
-    :col-type (or db-null text)
+    :col-type text
     :documentation "Lens type, manufacturer, etc."))
   (:metaclass dao-class)
   (:keys lens-id))
 
-(deftable sys-lens (!dao-def))
+(deftable sys-lens
+  (:create-sequence 'sys-lens-id-seq)
+  (!dao-def))
 
 (defclass sys-generic-device ()
   ((generic-device-id
-    :col-type integer)
+    :col-type integer
+    :col-default (:nextval 'sys-generic-device-id-seq))
    (camera-hardware-id
     :col-type (or db-null integer))
    (lens-id
@@ -127,18 +148,21 @@
   (:documentation "A row should describe either a camera with a lens, or a laser scanner."))
 
 (deftable sys-generic-device
+  (:create-sequence 'sys-generic-device-id-seq)
   (!dao-def)
   (!foreign 'sys-camera-hardware 'camera-hardware-id :on-delete :restrict :on-update :restrict)
   (!foreign 'sys-lens 'lens-id :on-delete :restrict :on-update :restrict)
+  ;;;; Once we have a sys-scanner table:
   ;;(!foreign 'sys-scanner 'scanner-id :on-delete :restrict :on-update :restrict)
   )
 
 (defclass sys-device-stage-of-life ()
   ((device-stage-of-life-id
-    :col-type integer)
+    :col-type integer
+    :col-default (:nextval 'sys-device-stage-of-life-seq))
    (recorded-device-id
     :col-type text
-    :documentation "Must be stored near to each data record.")
+    :documentation "Must be stored next to each data record.")
    (generic-device-id
     :col-type integer)
    (vehicle-name
@@ -161,7 +185,11 @@
   (:keys device-stage-of-life-id))
 
 (deftable sys-device-stage-of-life
+  (:create-sequence 'sys-device-stage-of-live-seq)
   (!dao-def)
+  (!index 'recorded-device-id)
+  (!index 'mounting-date)
+  (!index 'unmounting-date)
   (!foreign 'sys-generic-device 'generic-device-id :on-delete :restrict :on-update :restrict))
 
 (defclass sys-camera-calibration ()
@@ -173,7 +201,7 @@
    (person
     :col-type text)
    (main-description
-    :col-type (or db-null text)
+    :col-type text
     :documentation "Regarding this entire set of calibration data.  Note the special-purpose description fields inner-orientation-description, outer-orientation-description, boresight-description.")
    (debug
     :col-type boolean
@@ -185,7 +213,7 @@
     :col-type integer
     :documentation "Head up = 0; right ear up = 90; left ear up = -90; head down = 180.")
    (inner-orientation-description
-    :col-type (or db-null text)
+    :col-type text
     :documentation "Comments regarding inner orientation calibration.")
    (c
     :col-type double-float
@@ -221,7 +249,7 @@
     :col-type double-float
     :documentation "Inner orientation.")
    (outer-orientation-description
-    :col-type (or db-null text)
+    :col-type text
     :documentation "Comments regarding outer orientation calibration.")
    (dx
     :col-type double-float
@@ -242,7 +270,7 @@
     :col-type double-float
     :documentation "Outer orientation.")
    (boresight-description
-    :col-type (or db-null text)
+    :col-type text
     :documentation "Comments regarding boresight alignment calibration.")
    (bdx
     :col-type double-float
@@ -283,24 +311,12 @@
   (:metaclass dao-class)
   (:keys device-stage-of-life-id date))
 
-(deftable sys-camera-calibration (!dao-def)
+(deftable sys-camera-calibration
+  (!dao-def)
   (!foreign 'sys-device-stage-of-life 'device-stage-of-life-id :on-delete :restrict :on-update :restrict))
 
-
-;;(deftable data-template-point
-;;  (!dao-def)
-;;  (!foreign 'sys-measurements 'measurement-id :on-delete :cascade :on-update :cascade))
-;;
-;;
-;;
-;;
-;;
-;;(deftable data-template-image
-;;  (!dao-def)
-;;  (!foreign 'data-template-point 'point-id :on-delete :cascade :on-update :cascade))
-
 (defun create-all-sys-tables ()
-  "Create in current database a set of sys-* tables, i.e. tables for use by all projects.  The database should probably be empty."
+  "Create in current database a set of sys-* tables, i.e. tables that are used by all projects.  The database should probably be empty."
   (create-table 'sys-user)
   (create-table 'sys-project)
   (create-table 'sys-user-role)
@@ -311,14 +327,23 @@
   (create-table 'sys-device-stage-of-life)
   (create-table 'sys-camera-calibration))
 
+(defun !!index (table field &key (index-type :btree)) 
+  (format nil "CREATE INDEX ~0@*~A_~1@*~A_index ON ~0@*~A USING ~2@*~A (~1@*~A)"
+          (s-sql:to-sql-name table)
+          (s-sql:to-sql-name field)
+          (s-sql:to-sql-name index-type)))
+
 (defun create-data-table-definitions (common-table-name)
   "Define or redefine a bunch of dao-classes which can hold measuring data and which are connected to database tables named common-table-name plus type-specific prefix and/or suffix."
-  (let ((image-data-table-name (format nil "~A-image"  common-table-name))
-        (point-data-table-name (format nil "~A-point"  common-table-name)))
+  (let* ((table-prefix "data-")
+         (image-data-table-name (format nil "~A~A-image" table-prefix common-table-name))
+         (point-data-table-name (format nil "~A~A-point" table-prefix common-table-name))
+         (point-id-sequence-name (make-symbol (format nil "~A~A-point-id-seq" table-prefix common-table-name))))
     (eval
      `(defclass point-data ()
         ((point-id
-          :col-type integer)
+          :col-type integer
+          :col-default (:nextval ,point-id-sequence-name))
          (measurement-id
           :col-type integer)
          (trigger-time
@@ -351,51 +376,70 @@
           :col-type double-precision)
          (heading-sd
           :col-type double-precision)
-         (position
+         (coordinates
           :col-type geometry
           :documentation "Geographic coordinates."))
         (:metaclass dao-class)
         (:table-name ,point-data-table-name)
         (:keys point-id)
         (:documentation "There shouldn't be any point-id without an equal one in the *-image table.  This can't be enforced on database level.  Perhaps we should create some cleaning operation to maintain referential integrity. (TODO)")))
-    ;;(eval
-    ;; `(defclass image-data ()
-    ;;    ((point-id
-    ;;      :col-type integer)
-    ;;     (filename
-    ;;      :col-type text
-    ;;      :documentation "Name without any directory components.")
-    ;;     (byte-position
-    ;;      :col-type integer
-    ;;      :documentation "Start of image in .pictures file named by slot filename.")
-    ;;     (recorded-device-id
-    ;;      :col-type text
-    ;;      :documentation "As found in .pictures file, header tag `cam=´.")
-    ;;     (footprint
-    ;;      :col-type geometry
-    ;;      :documentation "Polygon on the ground describing the approximate area covered by this image.")
-    ;;     (gain
-    ;;      :col-type double-precision)
-    ;;     (shutter
-    ;;      :col-type double-precision))
-    ;;    (:metaclass dao-class)
-    ;;    (:table-name ,image-data-table-name)
-    ;;    (:keys point-id filename byte-position)
-    ;;    (:documentation "One row per image.")))
+    (deftable point-data
+      (:create-sequence point-id-sequence-name)
+      (!dao-def)
+      (!!index point-data-table-name 'measurement-id)
+      (!!index point-data-table-name 'trigger-time)
+      (!!index point-data-table-name 'coordinates :index-type :gist)
+      ;; The following let shouldn't be necessary. (Wart In !foreign.)
+      (let ((*table-symbol* point-data-table-name)
+            (*table-name*  (s-sql:to-sql-name point-data-table-name)))
+        (!foreign 'sys-measurement 'measurement-id :on-delete :cascade :on-update :cascade)))
     (eval
-     `(deftable ,point-data-table-name
-        (!dao-def)
-        (!foreign 'sys-measurements 'measurement-id :on-delete :cascade :on-update :cascade)))
-    ;;(eval
-    ;; `(deftable image-data
-    ;;    (!dao-def)
-    ;;    (!foreign ',point-data-table-name 'point-id :on-delete :cascade :on-update :cascade)))
-    (eval
-     `(create-table ',point-data-table-name))
-    ))
+     `(defclass image-data ()
+        ((point-id
+          :col-type integer)
+         (filename
+          :col-type text
+          :documentation "Name without any directory components.")
+         (byte-position
+          :col-type integer
+          :documentation "Start of image in .pictures file named by slot filename.")
+         (recorded-device-id
+          :col-type text
+          :documentation "As found in .pictures file, header tag `cam=´.")
+         (footprint
+          :col-type geometry
+          :documentation "Polygon on the ground describing the approximate area covered by this image.")
+         (gain
+          :col-type double-precision)
+         (shutter
+          :col-type double-precision))
+        (:metaclass dao-class)
+        (:table-name ,image-data-table-name)
+        (:keys point-id filename byte-position)
+        (:documentation "One row per image.")))
+    (deftable image-data
+      (!dao-def)
+      (!!index image-data-table-name 'recorded-device-id)
+      (!!index image-data-table-name 'gain)
+      (!!index image-data-table-name 'shutter)
+      (!!index image-data-table-name 'footprint :index-type :gist)
+      ;; The following let shouldn't be necessary. (Wart in !foreign.)
+      (let ((*table-symbol* image-data-table-name)
+            (*table-name*  (s-sql:to-sql-name image-data-table-name)))
+        (!foreign point-data-table-name 'point-id :on-delete :cascade :on-update :cascade)))))
 
 (defun create-data-tables (project-name &optional (common-table-name project-name))
   "Create in current database a (previously non-existing) set of canonically named tables.  common-table-name should in most cases resemble the project name and will be stored in table sys-project, field table-name."
   (create-data-table-definitions common-table-name)
-  ;;(ignore-errors (create-all-sys-tables)) ; TODO: only ignore certain errors
-  ;;(create-table 'image-data)
+  (handler-case (create-all-sys-tables) ; Create system tables if necessary.
+    (cl-postgres-error:syntax-error-or-access-violation () nil))
+  (when (select-dao 'sys-project (:= 'common-table-name
+                                     (s-sql:to-sql-name common-table-name)))
+    (error "There is already a row with a common_table_name of ~A in table ~A."
+           common-table-name (s-sql:to-sql-name (dao-table-name 'sys-project))))
+  (create-table 'point-data)
+  (create-table 'image-data)
+  (insert-dao
+   (make-instance 'sys-project
+                  :project-name project-name
+                  :common-table-name common-table-name)))
