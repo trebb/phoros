55b8add4c821485248ce821c7ddcbea24bee5fe9
Bert Burgemeister <trebbu@googlemail.com>
2011-06-08 16:43:39 +0200

    More shell script examples

---
+++ add-to-phoros-project.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+shopt -s extglob                # make !() available
+
+pg_credentials='--host=my_db_server --database=phoros_workspace --user=some_db_user --password=wonttell'
+
+cd phoros
+
+for i in /path/to/all/projects/one_project/!(already_done_01|already_done_02|already_done_03);
+do
+    ./phoros --store-images-and-points=this_acquisition_project --directory=$i/ --common-root=/path/to/all/projects/ $pg_credentials
+done
+
+./phoros --add-to-presentation-project=my_presentation_project --acquisition-project=this_acquisition_project $pg_credentials
